4e74304933004865d59951578132b2ca25145bbe
ParkEunwoo <dmsdn960@gmail.com>
2020-08-27 21:41:25 +0900

    passport 모듈 적용

---
+++ src/passport.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(
+  new LocalStrategy({ session: true }, (username, password, done) => {
+    try {
+      // 데이터베이스에서 일치하는 사용자를 찾는다
+      const user = users.filter(u => u.identify(username, password))[0]
+
+      // 콜백함수로 결과를 전달한다. 사용자가 없으면 false를 전달
+      done(null, user ? user : false)
+    } catch (err) {
+      // 예외가 발생하면 콜백함수에 오류를 전달한다
+      done(err)
+    }
+  })
+)
+
+module.exports = {
+  initPassport: (app) => {
+    app.use(passport.initialize());
+
+    app.use(passport.session());
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
 const server = require("http").createServer(app);
+const session = require('express-session');
+
+const {connectSocket} = require('./socket.js');
+const {initPassport} = require('./passport.js');
 
 const PORT = 3000;
 
 module.exports = {
   createServer: () => {
     app.use(express.static("public"));
+    app.use(session({
+      name: 'bugem-session',
+      secret: 'bugembugem',
+      resave: true,
+      saveUninitialized: true,
+    }))
+    connectSocket(server);
+    initPassport(app);
+
+    app.get("/debug", (req, res) => {
+      res.json({
+        "req.session": req.session, // 세션 데이터
+        "req.user": req.user, // 유저 데이터(뒷 부분에서 설명)
+        "req._passport": req._passport, // 패스포트 데이터(뒷 부분에서 설명)
+      })
+    })
 
     app.get('/', (req, res) => {
       res.sendFile(__dirname + 'public/index.html')
@@ -16,6 +36,5 @@ module.exports = {
       console.log(`listening ${PORT} port`);
     })
     
-    return server;
   }
 }
