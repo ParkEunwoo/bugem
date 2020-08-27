c54915842e02d0e46401366e5b5a5814513d4906
ParkEunwoo <dmsdn960@gmail.com>
2020-08-27 21:08:37 +0900

    3000번 포팅 시작

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const server = require("http").createServer(app);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`listening ${PORT} port`);
+})
