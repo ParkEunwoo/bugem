b6b51a5d7b72b45f139551a6729677a44e299d50
ParkEunwoo <dmsdn960@gmail.com>
2020-08-27 21:25:25 +0900

    소켓 클라이언트 설정

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {createServer} = require('./src/server.js');
 const {connectSocket} = require('./src/socket.js');
-console.log(createServer)
+
 const server = createServer();
 connectSocket(server);
