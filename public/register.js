5e0207b00422e31b716fab78ac33d0b0965a2bc1
ParkEunwoo <dmsdn960@gmail.com>
2020-08-29 00:16:21 +0900

    검증 클래스 추가

--- public/register.js
+++ public/register.js
@@ -3,6 +3,7 @@ const validation = {
   name: false,
   password: false
 }
+const $idContainer = document.getElementById('id-container');
 const $idInput = document.getElementById('id-input');
 const $idCheck = document.getElementById('id-check');
 $idInput.addEventListener('input', () => {
@@ -13,13 +14,16 @@ $idInput.addEventListener('input', () => {
     if(isDouble) {
       $idCheck.innerHTML = '중복된 아이디 입니다.'
       validation.id = false;
+      $idContainer.classList.remove('validated')
     } else {
       $idCheck.innerHTML = '사용가능한 아이디 입니다.'
       validation.id = true;
+      $idContainer.classList.add('validated')
     }
   })
 })
 
+const $nameContainer = document.getElementById('name-container');
 const $nameInput = document.getElementById('name-input');
 const $nameCheck = document.getElementById('name-check');
 $nameInput.addEventListener('input', () => {
@@ -30,13 +34,17 @@ $nameInput.addEventListener('input', () => {
     if(isDouble) {
       $nameCheck.innerHTML = '중복된 닉네임 입니다.'
       validation.name = false;
+      $nameContainer.classList.remove('validated')
     } else {
       $nameCheck.innerHTML = '사용가능한 닉네임 입니다.'
       validation.name = true;
+      $nameContainer.classList.add('validated')
     }
   })
 })
 
+const $passwordContainer = document.getElementById('password-container')
+const $repasswordContainer = document.getElementById('password-check-container');
 const $passwordInput = document.getElementById('password-input');
 const $repasswordInput = document.getElementById('repassword-input');
 const $passwordCheck = document.getElementById('password-check');
@@ -44,9 +52,13 @@ $repasswordInput.addEventListener('input', () => {
   if($passwordInput.value === $repasswordInput.value) {
     $passwordCheck.innerHTML = ''
     validation.password = true;
+    $passwordContainer.classList.remove('validated');
+    $repasswordContainer.classList.remove('validated');
   } else {
     $passwordCheck.innerHTML = '비밀번호를 확인해주세요';
     validation.password = false;
+    $passwordContainer.classList.add('validated');
+    $repasswordContainer.classList.add('validated');
   }
 })
 
