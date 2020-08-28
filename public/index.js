880c62020a7ec506d3aad26b28cb4941d9e52a37
ParkEunwoo <dmsdn960@gmail.com>
2020-08-28 21:47:06 +0900

    유저 관련 슽타일

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 const $loginInfo = document.getElementById('login-info');
 
 fetch('/auth/session').then(response => response.json()).then(data => {
-  $loginInfo.innerHTML = `<a href="/channel/create" class="create-channel">채널 생성</a><div class="user">${data}</div><form action="/auth/logout" method="post"><input type="submit" value="로그아웃"></form>`;
+  $loginInfo.innerHTML = `<a href="/channel/create" class="create-channel"><i class="material-icons">video_call</i></a><div class="user">${data}</div><form action="/auth/logout" method="post"><input type="submit" value="로그아웃" class="logout"></form>`;
 }).catch(e => {
-  $loginInfo.innerHTML = `<a href="auth/login">로그인/회원가입</a>`
+  $loginInfo.innerHTML = `<a href="auth/login" class="login">로그인</a>`
 })
 
 const $recommandContainer = document.getElementById('recommand-channel');
