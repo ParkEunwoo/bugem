29df5d28ec64be99698085a72cf1ee0aec459e2a
ParkEunwoo <dmsdn960@gmail.com>
2020-08-29 00:39:39 +0900

    이미지 입력 공간

--- public/create.js
+++ public/create.js
@@ -1,18 +1,24 @@
 function readURL(input) {
   if (input.files && input.files[0]) {
-   var reader = new FileReader();
+   const reader = new FileReader();
    
-   reader.onload = function (e) {
+  reader.onload = function (e) {
     $preview.setAttribute('src', e.target.result);  
-   }
-   
-   reader.readAsDataURL(input.files[0]);
-   }
- }
- 
-  const $imageInput = document.getElementById('thumbnail-input');
-  const $preview = document.getElementById('thumbnail-preview');
+  }
+  
+  reader.readAsDataURL(input.files[0]);
+  }
+}
 
-  $imageInput.addEventListener('change', (e) => {
-  readURL(e.target)
-  })
+const $imageInput = document.getElementById('thumbnail-input');
+const $preview = document.getElementById('thumbnail-preview');
+
+const $inputButton = document.getElementById('input-button');
+$inputButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  $imageInput.click();
+})
+
+$imageInput.addEventListener('change', (e) => {
+readURL(e.target)
+})
