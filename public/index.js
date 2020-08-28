836124b4cfad21f6f485720a1eb91b1a59a36688
ParkEunwoo <dmsdn960@gmail.com>
2020-08-28 15:35:15 +0900

    검색 마크업

--- public/index.js
+++ public/index.js
@@ -12,3 +12,22 @@ fetch('/channel/recommand-list').then(response => response.json()).then(data => 
     `, '')
   }
 })
+
+const $searchButton = document.getElementById('search-submit');
+const $searchInput = document.getElementById('search-input');
+const $searchContainer = document.getElementById('search-channel')
+
+$searchButton.addEventListener('click', () => {
+  fetch(`/channel/search/${$searchInput.value}`).then(response => response.json()).then(data => {
+    if(Array.isArray(data)) {
+      $searchContainer.innerHTML = data.reduce((htmlString, channel) => `${htmlString}
+        <a href="/channel/join/${channel.id}">
+          <div>
+            <h3>${channel.title}</h3>
+          
+          </div>
+        </a>
+      `, '')
+    }
+  })
+})
