d29a452b97c486d73fff44c79f16acb64fda810b
ParkEunwoo <dmsdn960@gmail.com>
2020-08-28 22:52:21 +0900

    이름 비밀번호 확인

--- public/register.js
+++ public/register.js
@@ -1,3 +1,8 @@
+const validation = {
+  id: false,
+  name: false,
+  password: false
+}
 const $idInput = document.getElementById('id-input');
 const $idCheck = document.getElementById('id-check');
 $idInput.addEventListener('input', () => {
@@ -7,9 +12,39 @@ $idInput.addEventListener('input', () => {
   fetch(`/auth/check/userid/${$idInput.value}`).then(response => response.json()).then(isDouble => {
     if(isDouble) {
       $idCheck.innerHTML = '중복된 아이디 입니다.'
+      validation.id = false;
     } else {
       $idCheck.innerHTML = '사용가능한 아이디 입니다.'
+      validation.id = true;
     }
   })
 })
 
+const $nameInput = document.getElementById('name-input');
+const $nameCheck = document.getElementById('name-check');
+$nameInput.addEventListener('input', () => {
+  if($nameInput.value === '') {
+    return;
+  }
+  fetch(`/auth/check/name/${$nameInput.value}`).then(response => response.json()).then(isDouble => {
+    if(isDouble) {
+      $nameCheck.innerHTML = '중복된 닉네임 입니다.'
+      validation.name = false;
+    } else {
+      $nameCheck.innerHTML = '사용가능한 닉네임 입니다.'
+      validation.name = true;
+    }
+  })
+})
+
+const $passwordInput = document.getElementById('password-input');
+const $repasswordInput = document.getElementById('repassword-input');
+const $passwordCheck = document.getElementById('password-check');
+$repasswordInput.addEventListener('input', () => {
+  if($passwordInput.value === $repasswordInput.value) {
+    validation.password = true;
+  } else {
+    $passwordCheck.innerHTML = '비밀번호를 확인해주세요';
+    validation.password = false;
+  }
+})
