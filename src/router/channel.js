a9d113810b2f32bdacb8a2ab88976f717e6041bc
ParkEunwoo <dmsdn960@gmail.com>
2020-08-28 14:53:03 +0900

    채널 생성 마크업

--- src/router/channel.js
+++ src/router/channel.js
@@ -4,13 +4,16 @@ const {isAuthenticated} = require('./../passport');
 const Channel = require('../model/Channel.js')
 const path = require('path')
 
+router.get('/create', isAuthenticated(), (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../public/createChannel.html'));
+})
 router.post("/create", isAuthenticated(), (req, res) => {
   const channel = new Channel({title:'제목', category:'분위기', thumbnail:'/ghn.png'}, req.user);
 
   res.redirect(`/channel/join/${channel.id}`);
 });
 
-router.get("/join/:channelId", (req, res) => {
+router.get("/join/:channelId", isAuthenticated(), (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../public/channel.html"));
 });
 
