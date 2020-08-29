f980094c225b24ceb3e59b011a5dd8b744f0888d
ParkEunwoo <dmsdn960@gmail.com>
2020-08-29 17:02:08 +0900

    https 적용

--- src/server.js
+++ src/server.js
@@ -1,26 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const session = require('express-session');
-const path = require('path');
+const https = require("https");
+const fs = require("fs");
+const http = require("http");
+const session = require("express-session");
+const path = require("path");
 
-const {connectSocket} = require('./socket.js');
-const {initPassport} = require('./passport.js');
-const db = require('./db.js');
+const options = {
+  ca: fs.readFileSync("/etc/letsencrypt/live/showyourstory.tk/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/showyourstory.tk/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/showyourstory.tk/cert.pem"),
+};
 
-const PORT = 3000;
+http.createServer(app).listen(80);
+const server = https.createServer(options, app);
+
+const { connectSocket } = require("./socket.js");
+const { initPassport } = require("./passport.js");
+const db = require("./db.js");
+
+const PORT = 443;
 
 module.exports = {
   createServer: () => {
     app.use(express.static("public"));
-    app.use('/thumbnail', express.static('uploads'));
-    app.use(session({
-      name: 'bugem-session',
-      secret: 'bugembugem',
-      resave: true,
-      saveUninitialized: true,
-    }))
-    app.use(express.urlencoded({ extended: true }))
+    app.use("/thumbnail", express.static("uploads"));
+    app.use(
+      session({
+        name: "bugem-session",
+        secret: "bugembugem",
+        resave: true,
+        saveUninitialized: true,
+      })
+    );
+    app.use(express.urlencoded({ extended: true }));
     connectSocket(server);
 
     const passport = initPassport();
@@ -28,24 +41,21 @@ module.exports = {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.use('/auth', require('./router/auth.js'));
-    app.use('/channel', require('./router/channel'));
+    app.use("/auth", require("./router/auth.js"));
+    app.use("/channel", require("./router/channel"));
     app.get("/debug", (req, res) => {
       res.json({
         "req.session": req.session, // 세션 데이터
         "req.user": req.user, // 유저 데이터(뒷 부분에서 설명)
         "req._passport": req._passport, // 패스포트 데이터(뒷 부분에서 설명)
-      })
-    })
+      });
+    });
 
-    app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../public/index.html'));
-    })
-
-    
+    app.get("/", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../public/index.html"));
+    });
     server.listen(PORT, () => {
       console.log(`listening ${PORT} port`);
-    })
-    
-  }
-}
+    });
+  },
+};
